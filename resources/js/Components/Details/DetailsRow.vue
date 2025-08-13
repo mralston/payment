14d060b6a8e26791120139cabc6476c05642843f
Matthew Jameson <matthew.jameson@projectbetterenergy.com>
2025-08-13 16:39:10 +0100

    Add min height

--- resources/js/Components/Details/DetailsRow.vue
+++ resources/js/Components/Details/DetailsRow.vue
@@ -9,7 +9,7 @@ const props = defineProps({
 </script>
 
 <template>
-    <div class="flex flex-row gap-2 h-10 items-center pl-1" :class="{ 'flex-col': stack, 'border-b border-gray-200': !stack }">
+    <div class="flex flex-row gap-2 min-h-10 items-center pl-1" :class="{ 'flex-col': stack, 'border-b border-gray-200': !stack }">
         <div class="w-2/3 font-bold flex flex-row gap-2 items-center" :class="{ 'w-full': stack }">
             <Icon v-if="icon" :icon="icon" class="mr-2" />
             {{ label }}
