144d07c4252930dcc5ef4e71c4bbd1e096866234
Matt Ralston <matt@mralston.co.uk>
2025-08-17 21:43:21 +0100

    Add Tailwind CSS and PostCSS setup to payment package
    Integrated Tailwind CSS with a basic configuration targeting Vue files, along with PostCSS using autoprefixer. Updated dependencies in `package.json` and included styles import in `app.js` to enable styling functionality.

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './resources/js/**/*.vue',
+    './resources/js/**/*.js',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
