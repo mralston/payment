6e45b887a4d963388c5ab64f671e9b1360f846d6
Matthew Jameson <matthew.jameson@projectbetterenergy.com>
2025-09-17 12:58:10 +0100

    Hometree statuses

--- src/Enums/PaymentStatus.php
+++ src/Enums/PaymentStatus.php
@@ -21,4 +21,22 @@ enum PaymentStatus: string
     case PENDING = 'pending';
     case REFERRED = 'referred';
     case SNAGGED = 'snagged';
+
+    case HOMETREE_PENDING_APPLICANTS = 'pending-applicants';
+    case HOMETREE_PENDING_CUSTOMER_CHOICE = 'pending-customer-choice';
+    case HOMETREE_PENDING_UNDERWRITING_REVIEW = 'pending-underwriting-review';
+    case HOMETREE_PENDING_CUSTOMER_DATA = 'pending-customer-data';
+    case HOMETREE_PENDING_CUSTOMER_AGREEMENT = 'pending-customer-agreement';
+    case HOMETREE_PENDING_DOCUMENTATION_REVIEW = 'pending-documentation-review';
+    case HOMETREE_PENDING_INSTALLATION = 'pending-installation';
+    case HOMETREE_PENDING_QUOTE_REVIEW = 'pending-quote-review';
+    case HOMETREE_PROCESSING = 'processing';
+    case HOMETREE_ACTIVE_INSTALLED = 'active-installed';
+    case HOMETREE_ACTIVE_COMPLETED = 'active-completed';
+    case HOMETREE_FINAL_DECLINED = 'final-declined';
+    case HOMETREE_FINAL_CANCELLED = 'final-cancelled';
+    case HOMETREE_FINAL_ENDED = 'final-ended';
+    case HOMETREE_FINAL_ABANDONED = 'final-abandoned';
+    case HOMETREE_FINAL_ARCHIVED = 'final-archived';
+    case HOMETREE_INVALID = 'invalid';
 }
