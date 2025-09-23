e83706b1c150b593cbd92d29243d26e12167bc3d
Matt Ralston <matt@mralston.co.uk>
2025-09-23 09:45:56 +0100

    Update cash deposit when system cost changes.

--- src/Providers/EventServiceProvider.php
+++ src/Providers/EventServiceProvider.php
@@ -4,13 +4,18 @@ namespace Mralston\Payment\Providers;
 
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 use Mralston\Payment\Events\PaymentCancelled;
+use Mralston\Payment\Events\PriceChanged;
 use Mralston\Payment\Listeners\NotifyPaymentProviderOfCancellation;
+use Mralston\Payment\Listeners\UpdateCashDeposit;
 
 class EventServiceProvider extends ServiceProvider
 {
     protected $listen = [
         PaymentCancelled::class => [
             NotifyPaymentProviderOfCancellation::class,
+        ],
+        PriceChanged::class => [
+            UpdateCashDeposit::class,
         ],
     ];
 
